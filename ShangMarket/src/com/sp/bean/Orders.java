e5c1f1fa071d801371882c3aa2a05dcb2c0a7e35
GhostZXY <1176924164@qq.com>
2021-08-25 16:28:26 +0800

    尚品 ver1.1 框架搭建

---
+++ ShangMarket/src/com/sp/bean/Orders.java
@@ -0,0 +1,18 @@
+package com.sp.bean;
+
+import java.util.Date;
+
+public class Orders {
+	private int o_id;
+	private int u_id;
+	private int s_id;
+	private int p_id;
+	private int a_id;
+	private int o_num;
+	private int o_log_no;
+	private String o_status;
+	private String o_log_name;
+	private Date o_ordertime;
+	
+	
+}
